6684db314b7b66bc7697af9506537d375dc5c71f
Shady Sharaf <shady@sharaf.me>
2014-11-09 06:42:42 +0200

    Tweak page security params, and fix for Poodle's deprecated SSLv3

--- helper.js
+++ helper.js
@@ -96,6 +96,11 @@ function getNewPage() {
 	// Timeout
 	page.settings.resourceTimeout = 30000; // 30 seconds
 	page.settings.loadImages = false;
+	page.settings.localToRemoteUrlAccessEnabled = true;
+	page.settings.ignoreSslErrors = true;
+	page.settings.webSecurityEnabled = false;
+	page.settings.sslProtocol = 'tlsv1';
+
 	//page.onLoadStarted = onLoadStartedApplyTimeout;
 
 	return page;
